9659cf1113ea5b9382bc3d516b29d2ca9f13e5c7
kimbeomjun <gimb52043@gmail.com>
2023-06-11 18:31:05 +0900

    mdx

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,23 +5,36 @@ import { PageProps, graphql } from "gatsby";
 
 // 밑에 선언한 query명(BlogTitle)으로 타입 자동완성(BlogTitlesQuery)
 // Queries로 네임스페이스 사용
-export default function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
+export default function Blog({ data }: PageProps<Queries.BlogPostQuery>) {
   return (
     <Layout title="title : Blog">
-      <ul>
-        {data.allFile.nodes.map((file, index) => (
-          <li key={index}>{file.name}</li>
+      <section>
+        {data.allMdx.nodes.map((file, index) => (
+          <article key={index}>
+            <h1>{file.frontmatter?.title}</h1>
+            <h1>{file.frontmatter?.category}</h1>
+            <h1>{file.frontmatter?.data}</h1>
+            <h1>{file.frontmatter?.name}</h1>
+            <hr />
+            <h1>{file.excerpt}</h1>
+          </article>
         ))}
-      </ul>
+      </section>
     </Layout>
   );
 }
 
 export const query = graphql`
-  query BlogTitles {
-    allFile {
+  query BlogPost {
+    allMdx {
       nodes {
-        name
+        frontmatter {
+          title
+          category
+          data(formatString: "YYY.MM.DD")
+          name
+        }
+        excerpt(pruneLength:10)
       }
     }
   }
